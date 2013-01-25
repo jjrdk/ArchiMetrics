b2bbea7ee9edcd3fffef9a33ebaaa054d1f15e7f
cbakopanos_cp <SND\cbakopanos_cp>
2013-01-25 07:55:13 -0800

    do not allow parallel edges

---
+++ Main/ArchiCop/ViewModel/GraphEdgesViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using ArchiCop.Core;
+
+namespace ArchiCop.ViewModel
+{
+    public class GraphEdgesViewModel : GraphViewModel
+    {
+        public GraphEdgesViewModel(GraphEngine graphEngine, string displayName):
+            base(graphEngine, displayName)
+        {
+            EdgesToVisualize = graphEngine.Edges;
+        }
+
+        public IEnumerable<ArchiCopEdge> EdgesToVisualize { get; private set; }
+
+    }
+}
