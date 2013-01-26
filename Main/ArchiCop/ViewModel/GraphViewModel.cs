a1e5cb7cba4504cf225b5a21215eb220ede41435
cbakopanos_cp <SND\cbakopanos_cp>
2013-01-26 13:26:12 -0800

    graphs layout types

--- Main/ArchiCop/ViewModel/GraphViewModel.cs
+++ Main/ArchiCop/ViewModel/GraphViewModel.cs
@@ -4,6 +4,8 @@ namespace ArchiCop.ViewModel
 {
     public class GraphViewModel : WorkspaceViewModel
     {
+        private string _layoutAlgorithmType = "KK";
+
         public GraphViewModel(GraphEngine graphEngine, string displayName)
         {
             DisplayName = displayName;
@@ -11,6 +13,18 @@ namespace ArchiCop.ViewModel
             GraphToVisualize = graphEngine.Graph;
         }
 
+        public string LayoutAlgorithmType
+        {
+            get { return _layoutAlgorithmType; }
+            set
+            {
+                if (value != _layoutAlgorithmType)
+                {
+                    _layoutAlgorithmType = value;
+                    RaisePropertyChanged("LayoutAlgorithmType");                    
+                }
+            }
+        }
         public ArchiCopGraph GraphToVisualize { get; private set; }
     }
 }
