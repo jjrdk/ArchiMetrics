b2910002a2abeb2975d06913bbd5c251e3db1230
jjrdk_cp <SND\jjrdk_cp>
2013-05-19 12:28:07 -0700

    Added psake based build script for ArchiMeter.

---
+++ Main/tasks.ps1
@@ -0,0 +1,66 @@
+properties {
+	$configuration = "Release"
+	$platform = "Any CPU"
+	$folderPath = ".\"
+	$cleanPackages = $false
+	$oldEnvPath = ""
+}
+
+task default -depends CleanUpMsBuildPath
+
+task CleanUpMsBuildPath -depends Test {
+	if($oldEnvPath -ne "")
+	{
+		Write-Host "Reverting Path variable"
+		$Env:Path = $oldEnvPath
+	}
+}
+
+task Test -depends Compile, Clean { 
+}
+
+task Compile -depends UpdatePackages { 
+  Exec { C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "ArchiMeter.sln" }
+  'Executed Compile!'
+}
+
+task UpdatePackages -depends Clean {
+	$packageConfigs = Get-ChildItem -Path .\ -Include "packages.config" -Recurse
+	foreach($config in $packageConfigs){
+        #Write-Host $config.DirectoryName
+		.\.nuget\nuget.exe i $config.FullName -o packages -source https://nuget.org/api/v2/
+	}
+}
+
+task Clean -depends CheckMsBuildPath { 
+	Get-ChildItem $folderPath -include bin,obj -Recurse | foreach ($_) { remove-item $_.fullname -Force -Recurse }
+	if($cleanPackages -eq $true){
+		if(Test-Path "$folderPath\packages"){
+			Get-ChildItem "$folderPath\packages" -Recurse | Where { $_.PSIsContainer } | foreach ($_) { Write-Host $_.fullname; remove-item $_.fullname -Force -Recurse }
+		}
+	}
+	
+	if(Test-Path "$folderPath\BuildOutput"){
+		Get-ChildItem "$folderPath\BuildOutput" -Recurse | foreach ($_) { Write-Host $_.fullname; remove-item $_.fullname -Force -Recurse }
+	}
+}
+
+task CheckMsBuildPath {
+	$envPath = $Env:Path
+	if($envPath.Contains("C:\Windows\Microsoft.NET\Framework\v4.0") -eq $false)
+	{
+		if(Test-Path "C:\Windows\Microsoft.NET\Framework\v4.0.30319")
+		{
+			$oldEnvPath = $envPath
+			$Env:Path = $envPath + ";C:\Windows\Microsoft.NET\Framework\v4.0.30319"
+		}
+		else
+		{
+			throw "Could not determine path to MSBuild. Make sure you have .NET 4.0.30319 installed"
+		}
+	}
+}
+
+task ? -Description "Helper to display task info" {
+	Write-Documentation
+}
