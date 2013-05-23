3ac2185c9db51ccf48c58c12902e31ccfb408982
jjrdk <jacob@reimers.dk>
2013-05-23 08:32:15 +0200

    Added support for additional conditions.

--- Main/ArchiMeter.Reports/ExpressionHelpers.cs
+++ Main/ArchiMeter.Reports/ExpressionHelpers.cs
@@ -9,25 +9,32 @@ namespace ArchiMeter.Reports
 
 	internal static class ExpressionHelpers
 	{
-		public static Expression<Func<T, bool>> CreateQuery<T>(this IEnumerable<ProjectSettings> projectSettings)
+		public static Expression<Func<T, bool>> CreateQuery<T>(this IEnumerable<ProjectSettings> projectSettings, Func<ParameterExpression, Expression> additionalFilterFactory = null)
 			where T : DataSegment
 		{
 			var parameter = Expression.Parameter(typeof(T));
 			var filter = projectSettings.Aggregate<ProjectSettings, Expression>(
 				null,
 				(ex, s) =>
-					{
-						var comparison = Expression.AndAlso(
-							Expression.Equal(
-								Expression.Property(parameter, "ProjectName"),
-								Expression.Property(Expression.Constant(s), "Name")),
-							Expression.Equal(
-								Expression.Property(parameter, "Date"),
-								Expression.Property(Expression.Constant(s), "Date")));
-						return ex == null
-							       ? comparison
-							       : Expression.OrElse(ex, comparison);
-					});
+				{
+					var comparison = Expression.AndAlso(
+						Expression.Equal(
+							Expression.Property(parameter, "ProjectName"),
+							Expression.Property(Expression.Constant(s), "Name")),
+						Expression.Equal(
+							Expression.Property(parameter, "Date"),
+							Expression.Property(Expression.Constant(s), "Date")));
+					return ex == null
+							   ? comparison
+							   : Expression.OrElse(ex, comparison);
+				});
+			if (additionalFilterFactory != null)
+			{
+				var additionalFilter = additionalFilterFactory(parameter);
+				filter = additionalFilter == null
+				? filter
+				: Expression.AndAlso(filter, additionalFilter);
+			}
 
 			var lambda = Expression.Lambda<Func<T, bool>>(filter, parameter);
 			return lambda;
