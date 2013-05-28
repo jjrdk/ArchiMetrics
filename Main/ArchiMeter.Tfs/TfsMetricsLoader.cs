88f8b923c4e3a8773ac764b3f9e00de5b293607e
jjrdk <jacob@reimers.dk>
2013-05-28 17:24:02 +0200

    Fixed metrics loading query.

--- Main/ArchiMeter.Tfs/TfsMetricsLoader.cs
+++ Main/ArchiMeter.Tfs/TfsMetricsLoader.cs
@@ -187,20 +187,19 @@
 
 		private IEnumerable<TfsBuild> GetBuilds(string teamProject, DateTime revisionTime)
 		{
-			const string Query = @"SELECT TOP 1 max([ID]) as ID
-      ,BuildName
-      ,TeamProject
-      ,min(DATEDIFF(SECOND, BuildTime, @DateParam)) as ClosestBuild
-  FROM [TFS_Metrics].[dbo].[Build]
-  WHERE BuildName LIKE @Project + '%' AND DATEDIFF(SECOND, BuildTime, @DateParam) >= 0
-  GROUP BY BuildName, TeamProject
-  ORDER BY ClosestBuild";
+			const string Query = @"SELECT TOP 1 Build.ID, BuildName, TeamProject, MAX(BuildTime) AS MaxBuildTime
+FROM Build
+INNER JOIN Metric ON Build.ID = Metric.BuildID
+WHERE BuildName LIKE @BuildDef + '%' and Metric.MetricType = 8
+and CreatedTS <= @DateParam
+GROUP BY Build.ID, BuildName, TeamProject
+ORDER BY MaxBuildTime DESC";
 			using (var databaseConnection = _connectionFactory.Create())
 			{
 				using (var cmd = new SqlCommand(Query, databaseConnection))
 				{
 					cmd.Parameters.AddWithValue("@DateParam", revisionTime);
-					cmd.Parameters.AddWithValue("@Project", teamProject);
+					cmd.Parameters.AddWithValue("@BuildDef", teamProject);
 
 					databaseConnection.Open();
 					using (var reader = cmd.ExecuteReader())
@@ -214,7 +213,7 @@
 										BuildId = reader.GetInt32(0),
 										BuildName = reader.GetString(1),
 										TeamProject = reader.GetString(2),
-										BuildTime = DateTime.UtcNow.AddSeconds(-reader.GetInt32(3))
+										BuildTime = reader.GetDateTime(3)
 									});
 						}
 
