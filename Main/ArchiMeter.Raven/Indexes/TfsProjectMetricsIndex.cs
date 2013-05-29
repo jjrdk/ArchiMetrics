0baf6855eca2647d8f463f6d4ecd757341e99690
jjrdk <jacob@reimers.dk>
2013-05-29 12:25:06 +0200

    Added index.

--- Main/ArchiMeter.Raven/Indexes/TfsProjectMetricsIndex.cs
+++ Main/ArchiMeter.Raven/Indexes/TfsProjectMetricsIndex.cs
@@ -2,6 +2,7 @@
 {
 	using System.Linq;
 	using Common.Documents;
+
 	using global::Raven.Client.Indexes;
 
 	public class TfsProjectMetricsIndex : AbstractIndexCreationTask<TfsMetricsDocument, TfsProjectMetrics>
@@ -9,71 +10,26 @@
 		public TfsProjectMetricsIndex()
 		{
 			Map = docs => from doc in docs
-							   from namespaceMetric in doc.Metrics
-							   from typeMetric in namespaceMetric.TypeMetrics
-							   select new
-										  {
-											  typeMetric.CyclomaticComplexity,
-											  typeMetric.LinesOfCode,
-											  typeMetric.MaintainabilityIndex,
-											  doc.ProjectName
-										  };
+						  from namespaceMetric in doc.Metrics
+						  from typeMetric in namespaceMetric.TypeMetrics
+						  select new
+									 {
+										 typeMetric.CyclomaticComplexity,
+										 typeMetric.LinesOfCode,
+										 typeMetric.MaintainabilityIndex,
+										 doc.ProjectName
+									 };
 
 			Reduce = docs => from doc in docs
-								  group doc by doc.ProjectName
-									  into project
-									  select new
-												 {
-													 CyclomaticComplexity = project.Sum(x => x.CyclomaticComplexity),
-													 LinesOfCode = project.Sum(x => x.LinesOfCode),
-													 MaintainabilityIndex = project.Sum(x => x.MaintainabilityIndex) / project.Sum(x => 1),
-													 ProjectName = project.Key
-												 };
+							 group doc by doc.ProjectName
+								 into project
+								 select new
+											{
+												CyclomaticComplexity = project.Sum(x => x.CyclomaticComplexity),
+												LinesOfCode = project.Sum(x => x.LinesOfCode),
+												MaintainabilityIndex = project.Sum(x => x.MaintainabilityIndex) / project.Sum(x => 1),
+												ProjectName = project.Key
+											};
 		}
 	}
-
-	//public class TfsProductionProjectMetricsIndex : AbstractIndexCreationTask<TfsMetricsDocument, TfsProjectMetrics>
-	//{
-	//	private static readonly string[] TestNames = new[]
-	//									  {
-	//										"Tests", 
-	//										"TestInterceptor", 
-	//										"Testing", 
-	//										"SchedulerTest", 
-	//										"Demo", 
-	//										"UnitTest", 
-	//										"Mock", 
-	//										"DevTest", 
-	//										"CodedUI", 
-	//										"Fakes", 
-	//										"Simulator"
-	//									  };
-
-	//	public TfsProductionProjectMetricsIndex()
-	//	{
-	//		Map = docs => from doc in docs
-	//						   from namespaceMetric in doc.Metrics
-	//						   from typeMetric in namespaceMetric.TypeMetrics
-	//						   where !typeMetric.Name.Contains("Tests")
-	//						   && !typeMetric.Name.Contains("TestInterceptor")
-	//						   select new
-	//									  {
-	//										  typeMetric.CyclomaticComplexity,
-	//										  typeMetric.LinesOfCode,
-	//										  typeMetric.MaintainabilityIndex,
-	//										  doc.ProjectName
-	//									  };
-
-	//		Reduce = docs => from doc in docs
-	//							  group doc by doc.ProjectName
-	//								  into project
-	//								  select new
-	//											 {
-	//												 CyclomaticComplexity = project.Sum(x => x.CyclomaticComplexity),
-	//												 LinesOfCode = project.Sum(x => x.LinesOfCode),
-	//												 MaintainabilityIndex = project.Sum(x => x.MaintainabilityIndex) / project.Sum(x => 1),
-	//												 ProjectName = project.Key
-	//											 };
-	//	}
-	//}
 }
---
+++ Main/ArchiMeter.Raven/Indexes/TfsProjectNameIndex.cs
@@ -0,0 +1,23 @@
+﻿namespace ArchiMeter.Raven.Indexes
+{
+	using System.Linq;
+	using ArchiMeter.Common.Documents;
+	using global::Raven.Abstractions.Indexing;
+	using global::Raven.Client.Indexes;
+
+	public class TfsProjectNameIndex : AbstractIndexCreationTask<TfsMetricsDocument>
+	{
+		public TfsProjectNameIndex()
+		{
+			this.Map = docs => from doc in docs
+			              select new
+				                     {
+					                     doc.ProjectName,
+					                     doc.MetricsDate,
+					                     doc.ProjectVersion
+				                     };
+			
+			StoreAllFields(FieldStorage.Yes);
+		}
+	}
+}
