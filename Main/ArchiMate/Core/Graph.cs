719c29827c140d5c68bee11339aa324d538a4e44
cbakopanos_cp <SND\cbakopanos_cp>
2013-06-06 04:12:46 -0700

    archimate.... filtering improove

--- Main/ArchiMate/Core/Graph.cs
+++ Main/ArchiMate/Core/Graph.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
-namespace ArchiMate
+namespace ArchiMate.Core
 {
     public class Graph<T>        
     {
--- Main/ArchiMate/Program.cs
+++ Main/ArchiMate/Program.cs
@@ -1,12 +1,23 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Windows.Forms;
+using ArchiMate.Core;
 
 namespace ArchiMate
 {
     internal class Program
     {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
         private static void Main(string[] args)
         {
-            Application.Run(new MainForm());
+            IVisualStudioProjectRepository projectRepository= new VisualStudioProjectRepository();
+            IVisualStudioSolutionRepository solutionRepository=new VisualStudioSolutionRepository();
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm(projectRepository, solutionRepository));
         }
     }
 }
