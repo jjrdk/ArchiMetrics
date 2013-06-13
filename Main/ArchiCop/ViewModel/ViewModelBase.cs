12ba2731da1fa44806b537416c0bb60d62cbd99e
cbakopanos <cbakopanos@gmail.com>
2013-06-13 19:14:32 +0200

    changes in namespaces for archicop

--- Main/ArchiCop/ViewModel/ViewModelBase.cs
+++ Main/ArchiCop/ViewModel/ViewModelBase.cs
@@ -1,5 +1,5 @@
-﻿using MvvmFoundation.Wpf;
-using System.Diagnostics;
+﻿using System.Diagnostics;
+using MvvmFoundation.Wpf;
 
 namespace ArchiCop.ViewModel
 {
@@ -45,9 +45,9 @@ namespace ArchiCop.ViewModel
         }
 
 #if DEBUG
-    /// <summary>
-    ///     Useful for ensuring that ViewModel objects are properly garbage collected.
-    /// </summary>
+        /// <summary>
+        ///     Useful for ensuring that ViewModel objects are properly garbage collected.
+        /// </summary>
         ~ViewModelBase()
         {
             string msg = string.Format("{0} ({1}) ({2}) Finalized", GetType().Name, DisplayName, GetHashCode());
