56fa4b60acc893db644e192670e94ea497c352d0
jjrdk <jacob@reimers.dk>
2013-06-20 14:58:16 +0200

    Removed test.

--- Main/ArchiMeter.ScriptPack.Tests/ArchiToolsTests.cs
+++ Main/ArchiMeter.ScriptPack.Tests/ArchiToolsTests.cs
@@ -11,12 +11,5 @@
 			var tools = new IdeTools();
 			tools.OpenProjects(@"..\..\");
 		}
-
-		[Test]
-		public void CanStartMergedSolutionsAsSingleSolution()
-		{
-			var tools = new IdeTools();
-			tools.OpenProjects(@"C:\Dev\Tfs\NewGen.Dev\Units\");
-		}
     }
 }
