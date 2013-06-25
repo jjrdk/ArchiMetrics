11eac5c32b22eca4b94d838ee62a54e79ddea708
jjrdk <jacob@reimers.dk>
2013-06-25 15:03:59 +0200

    Removed F# code.

--- ArchiMeter.Analysis.FS/PathFilter.fs
+++
@@ -1,6 +0,0 @@
-﻿namespace ArchiMeter.Analysis
-open System
-open ArchiMeter.Common
-
-type PathFilter(filter : Func<ProjectDefinition, bool>) =
-    member this.Filter(path : ProjectDefinition) = filter.Invoke path
