1f268d6a59d4f8518d5bb54e28888556fa230ad0
jjrdk <jacob@reimers.dk>
2013-07-16 10:38:02 +0200

    Documentation cleanup.

--- ArchiMetrics.Analysis/Metrics/MemberNameResolver.cs
+++ ArchiMetrics.Analysis/Metrics/MemberNameResolver.cs
@@ -2,7 +2,7 @@
 // <copyright file="MemberNameResolver.cs" company="Reimers.dk">
 //   Copyright © Reimers.dk 2012
 //   This source is subject to the Microsoft Public License (Ms-PL).
-//   Please see http://go.microsoft.com/fwlink/?LinkID=131993] for details.
+//   Please see http://go.microsoft.com/fwlink/?LinkID=131993 for details.
 //   All other rights reserved.
 // </copyright>
 // <summary>
