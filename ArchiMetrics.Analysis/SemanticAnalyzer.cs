fa9281c230f16ed031e37842be8f5c97cfefbb26
jjrdk <jacob@reimers.dk>
2013-10-18 16:00:07 +0200

    Removed string and using token.

--- ArchiMetrics.Analysis/SemanticAnalyzer.cs
+++ ArchiMetrics.Analysis/SemanticAnalyzer.cs
@@ -1,12 +1,9 @@
 namespace ArchiMetrics.Analysis
 {
-	using System;
 	using System.Collections.Generic;
 	using System.Linq;
-	using System.Threading.Tasks;
 	using Roslyn.Compilers.Common;
 	using Roslyn.Compilers.CSharp;
-	using Roslyn.Services;
 
 	public class SemanticAnalyzer
 	{
@@ -47,52 +44,11 @@ namespace ArchiMetrics.Analysis
 				{
 					var bodyNodes = method.Body.ChildNodes();
 					var dataflow = _model.AnalyzeDataFlow(bodyNodes.First(), bodyNodes.Last());
-					var hasThisReference = dataflow.DataFlowsIn.Any(x => x.Kind == CommonSymbolKind.Parameter && x.Name == "this");
+					var hasThisReference = dataflow.DataFlowsIn
+						.Any(x => x.Kind == CommonSymbolKind.Parameter && x.Name == Syntax.Token(SyntaxKind.ThisKeyword).ToFullString());
 					return !hasThisReference;
 				})
 				.ToArray();
-		}
-
-		private static IDataFlowAnalysis GetDataFlow(
-			CommonSyntaxNode start,
-			CommonSyntaxNode end,
-			ISemanticModel semanticModel)
-		{
-			var flow = semanticModel.AnalyzeDataFlow(start, end);
-			return flow.Succeeded
-				? flow
-				: null;
-		}
-
-		private static IControlFlowAnalysis GetControlFlow(CommonSyntaxNode start, CommonSyntaxNode end, ISemanticModel semanticModel)
-		{
-			var flow = semanticModel.AnalyzeControlFlow(start, end);
-			return flow.Succeeded
-				? flow
-				: null;
-		}
-
-		private static async Task<IEnumerable<T>> GetFlow<T>(
-			IDocument document,
-			Func<CommonSyntaxNode, CommonSyntaxNode, ISemanticModel, T> flowLoader)
-		{
-			var modelTask = document.GetSemanticModelAsync();
-			var methodsTask = document.GetSyntaxRootAsync();
-
-			await Task.WhenAll(modelTask, methodsTask);
-
-			var methods = methodsTask.Result
-				.DescendantNodes()
-				.OfType<MethodDeclarationSyntax>()
-				.Select(x => x.Body)
-				.Where(x => x != null)
-				.Select(x => x.ChildNodes())
-				.Where(x => x.Any())
-				.Select(x => flowLoader(x.First(), x.Last(), modelTask.Result))
-				.Where(x => x != null)
-				.ToArray();
-
-			return methods;
 		}
 	}
 }
