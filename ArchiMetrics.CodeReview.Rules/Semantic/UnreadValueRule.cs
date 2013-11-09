88b6da9f3795e5666154cdc2fd3936e77933259a
jjrdk <jacob@reimers.dk>
2013-11-09 20:06:43 +0100

    Added UnreadVariableRule.

---
+++ ArchiMetrics.CodeReview.Rules/Semantic/UnreadValueRule.cs
@@ -0,0 +1,76 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="UnreadValueRule.cs" company="Reimers.dk">
+//   Copyright © Reimers.dk 2012
+//   This source is subject to the Microsoft Public License (Ms-PL).
+//   Please see http://go.microsoft.com/fwlink/?LinkID=131993 for details.
+//   All other rights reserved.
+// </copyright>
+// <summary>
+//   Defines the UnreadValueRule type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ArchiMetrics.CodeReview.Rules.Semantic
+{
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Threading;
+	using ArchiMetrics.Common.CodeReview;
+	using Roslyn.Compilers.Common;
+	using Roslyn.Compilers.CSharp;
+	using Roslyn.Services;
+
+	internal abstract class UnreadValueRule : SemanticEvaluationBase
+	{
+		public override CodeQuality Quality
+		{
+			get { return CodeQuality.NeedsReview; }
+		}
+
+		public override QualityAttribute QualityAttribute
+		{
+			get { return QualityAttribute.CodeQuality | QualityAttribute.Maintainability; }
+		}
+
+		public override ImpactLevel ImpactLevel
+		{
+			get { return ImpactLevel.Type; }
+		}
+
+		protected abstract IEnumerable<ISymbol> GetSymbols(SyntaxNode node, ISemanticModel semanticModel);
+
+		protected override EvaluationResult EvaluateImpl(SyntaxNode node, ISemanticModel semanticModel, ISolution solution)
+		{
+			var references = GetSymbols(node, semanticModel)
+				.SelectMany(x => SymbolExtensions.FindReferences(x, solution, CancellationToken.None))
+				.SelectMany(x => x.Locations)
+				.Select(x => x.Location.SourceTree.GetRoot().FindToken(x.Location.SourceSpan.Start))
+				.Select(x => x.Parent)
+				.Where(x => x != null)
+				.Select(x => x.Parent)
+				.Where(IsNotAssignment)
+				.ToArray();
+
+			if (!references.Any())
+			{
+				return new EvaluationResult
+					   {
+						   Snippet = node.ToFullString()
+					   };
+			}
+
+			return null;
+		}
+
+		private static bool IsNotAssignment(CommonSyntaxNode syntax)
+		{
+			var expression = syntax as BinaryExpressionSyntax;
+			if (expression != null)
+			{
+				return expression.Kind != SyntaxKind.AssignExpression;
+			}
+
+			return true;
+		}
+	}
+}
--- ArchiMetrics.CodeReview.Rules/Semantic/UnreadVariableRule.cs
+++ ArchiMetrics.CodeReview.Rules/Semantic/UnreadVariableRule.cs
@@ -1,45 +1,44 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="UnreadFieldRule.cs" company="Reimers.dk">
+// <copyright file="UnreadVariableRule.cs" company="Reimers.dk">
 //   Copyright © Reimers.dk 2012
 //   This source is subject to the Microsoft Public License (Ms-PL).
 //   Please see http://go.microsoft.com/fwlink/?LinkID=131993 for details.
 //   All other rights reserved.
 // </copyright>
 // <summary>
-//   Defines the UnreadFieldRule type.
+//   Defines the UnreadVariableRule type.
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
-
-using System.Linq;
-using Roslyn.Compilers.Common;
-using Roslyn.Compilers.CSharp;
 
 namespace ArchiMetrics.CodeReview.Rules.Semantic
 {
 	using System.Collections.Generic;
+	using System.Linq;
+	using Roslyn.Compilers.Common;
+	using Roslyn.Compilers.CSharp;
 
-	internal class UnreadFieldRule : UnreadValueRule
+	internal class UnreadVariableRule : UnreadValueRule
 	{
 		public override SyntaxKind EvaluatedKind
 		{
-			get { return SyntaxKind.FieldDeclaration; }
+			get { return SyntaxKind.VariableDeclaration; }
 		}
 
 		public override string Title
 		{
-			get { return "Field is never read"; }
+			get { return "Variable is never read"; }
 		}
 
 		public override string Suggestion
 		{
-			get { return "Remove unread field."; }
+			get { return "Remove unread variable."; }
 		}
 
 		protected override IEnumerable<ISymbol> GetSymbols(SyntaxNode node, ISemanticModel semanticModel)
 		{
-			var declaration = (FieldDeclarationSyntax)node;
+			var declaration = (VariableDeclarationSyntax)node;
 
-			var symbols = declaration.Declaration.Variables.Select(x => semanticModel.GetDeclaredSymbol(x)).ToArray();
+			var symbols = declaration.Variables.Select(x => semanticModel.GetDeclaredSymbol(x)).ToArray();
 
 			return symbols;
 		}
