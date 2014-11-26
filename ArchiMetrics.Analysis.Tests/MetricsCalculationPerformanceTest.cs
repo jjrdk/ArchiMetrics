f48f2c18fde3d0223f80f166c6e1653f9ac32a5a
jjrdk <jacob@reimers.dk>
2014-11-26 06:55:46 +0100

    Added test for project only analysis.

--- ArchiMetrics.Analysis.Tests/MetricsCalculationPerformanceTest.cs
+++ ArchiMetrics.Analysis.Tests/MetricsCalculationPerformanceTest.cs
@@ -17,6 +17,7 @@ namespace ArchiMetrics.Analysis.Tests
 	using System.Threading.Tasks;
 	using ArchiMetrics.Analysis.Metrics;
 	using ArchiMetrics.Common;
+	using ArchiMetrics.Common.Metrics;
 	using metrics;
 	using Microsoft.CodeAnalysis;
 	using Microsoft.CodeAnalysis.MSBuild;
@@ -33,13 +34,12 @@ namespace ArchiMetrics.Analysis.Tests
 		}
 
 		[Test]
-		public async Task MeasurePerformance()
+		public async Task MeasureSolutionAnalysisPerformance()
 		{
 			using (var workspace = MSBuildWorkspace.Create())
 			{
 				var path = @"..\..\..\archimetrics.sln".GetLowerCaseFullPath();
-				var solution = await workspace.OpenSolutionAsync(path)
-										 .ConfigureAwait(false);
+				var solution = await workspace.OpenSolutionAsync(path).ConfigureAwait(false);
 				var metrics = new metrics.Metrics();
 				var timer = metrics.Timer(GetType(), "test", TimeUnit.Seconds, TimeUnit.Seconds);
 				for (var i = 0; i < 5; i++)
@@ -51,11 +51,36 @@ namespace ArchiMetrics.Analysis.Tests
 			}
 		}
 
+		[Test]
+		public async Task MeasureProjectAnalysisPerformance()
+		{
+			using (var workspace = MSBuildWorkspace.Create())
+			{
+				var path = @"..\..\..\ArchiMetrics.Common\ArchiMetrics.Common.csproj".GetLowerCaseFullPath();
+				var project = await workspace.OpenProjectAsync(path).ConfigureAwait(false);
+				var metrics = new metrics.Metrics();
+				var timer = metrics.Timer(GetType(), "test", TimeUnit.Seconds, TimeUnit.Seconds);
+				for (var i = 0; i < 5; i++)
+				{
+					var amount = timer.Time(() => PerformReview(project).Result);
+				}
+
+				Assert.Less(timer.Mean, 90.0);
+			}
+		}
+
 		private async Task<int> PerformReview(Solution solution)
 		{
 			var results = await _calculator.Calculate(solution).ConfigureAwait(false);
 			var amount = results.ToArray();
 			return amount.Length;
+		}
+
+		private async Task<IProjectMetric> PerformReview(Project project)
+		{
+			var results = await _calculator.Calculate(project, null).ConfigureAwait(false);
+
+			return results;
 		}
 	}
 }
