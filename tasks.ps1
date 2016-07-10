f32f0665161624aeb797e307ba8ded15f16d09eb
jjrdk <jacob@reimers.dk>
2016-07-10 14:21:01 +0200

    Convert solution to dnx

--- tasks.ps1
+++ tasks.ps1
@@ -19,8 +19,9 @@ task CleanUpMsBuildPath -depends BuildPackages {
 }
 
 task BuildPackages -depends Test {
-	Exec { .\.nuget\nuget.exe pack ArchiMetrics.Analysis.nuspec }
-	Exec { .\.nuget\nuget.exe pack ArchiMetrics.Analysis.symbols.nuspec -Symbols }
+	Exec { dnu pack ** --configuration $configuration}
+#	Exec { .\.nuget\nuget.exe pack ArchiMetrics.Analysis.nuspec }
+#	Exec { .\.nuget\nuget.exe pack ArchiMetrics.Analysis.symbols.nuspec -Symbols }
 }
 
 task Test -depends Compile, Clean {
@@ -28,35 +29,35 @@ task Test -depends Compile, Clean {
 	foreach($fwk in $fwkVersions) {
 		Write-Host "Building v. $fwk"
 		$output = ".\$buildOutput\$fwk\$configuration"
-		$common = Resolve-Path "$output\ArchiMetrics.Common.Tests.dll"
 		$analysis = Resolve-Path "$output\ArchiMetrics.Analysis.Tests.dll"
 		$codereview = Resolve-Path "$output\ArchiMetrics.CodeReview.Rules.Tests.dll"
 		
-		Exec { .\packages\NUnit.Runners.2.6.4\tools\nunit-console.exe $analysis }
-		Exec { .\packages\NUnit.Runners.2.6.4\tools\nunit-console.exe $codereview }
-		Exec { .\packages\NUnit.Runners.2.6.4\tools\nunit-console.exe $common }
+		Exec { .\packages\xunit.runner.console\2.1.0\tools\xunit-console.exe $analysis }
+		Exec { .\packages\xunit.runner.console\2.1.0\tools\xunit-console.exe $codereview }
 	}
 }
 
 task Compile -depends UpdatePackages {
-	$msbuild = Resolve-Path "${Env:ProgramFiles(x86)}\MSBuild\14.0\Bin\MSBuild.exe"
-	foreach($fwk in $fwkVersions) {
-		$output = "..\$buildOutput\$fwk\$configuration"
-		$options = "/p:configuration=$configuration;platform=$platform;TargetFrameworkVersion=v$fwk;OutputPath=$output"
-		Exec { & $msbuild ArchiMetrics.sln $options }
-	}
+	Exec { dnu build ** --configuration $configuration }
+#	$msbuild = Resolve-Path "${Env:ProgramFiles(x86)}\MSBuild\14.0\Bin\MSBuild.exe"
+#	foreach($fwk in $fwkVersions) {
+#		$output = "..\$buildOutput\$fwk\$configuration"
+#		$options = "/p:configuration=$configuration;platform=$platform;TargetFrameworkVersion=v$fwk;OutputPath=$output"
+#		Exec { & $msbuild ArchiMetrics.sln $options }
+#	}
 	'Executed Compile!'
 }
 
 task UpdatePackages -depends Clean {
-	$packageConfigs = Get-ChildItem -Path .\ -Include "packages.config" -Recurse
-	foreach($config in $packageConfigs){
-		#Write-Host $config.DirectoryName
-		Exec { .\.nuget\nuget.exe i $config.FullName -o packages -source https://nuget.org/api/v2/ }
-	}
+	Exec { & dnu restore}
+#	$packageConfigs = Get-ChildItem -Path .\ -Include "packages.config" -Recurse
+#	foreach($config in $packageConfigs){
+#		#Write-Host $config.DirectoryName
+#		Exec { .\.nuget\nuget.exe i $config.FullName -o packages -source https://nuget.org/api/v2/ }
+#	}
 }
 
-task Clean -depends CheckMsBuildPath { 
+task Clean { #-depends CheckMsBuildPath { 
 	Get-ChildItem $folderPath -include bin,obj -Recurse | foreach ($_) { remove-item $_.fullname -Force -Recurse }
 	if($cleanPackages -eq $true){
 		if(Test-Path "$folderPath\packages"){
@@ -69,21 +70,21 @@ task Clean -depends CheckMsBuildPath {
 	}
 }
 
-task CheckMsBuildPath {
-	$envPath = $Env:Path
-	if($envPath.Contains("C:\Windows\Microsoft.NET\Framework\v4.0") -eq $false)
-	{
-		if(Test-Path "C:\Windows\Microsoft.NET\Framework\v4.0.30319")
-		{
-			$oldEnvPath = $envPath
-			$Env:Path = $envPath + ";C:\Windows\Microsoft.NET\Framework\v4.0.30319"
-		}
-		else
-		{
-			throw "Could not determine path to MSBuild. Make sure you have .NET 4.0.30319 installed"
-		}
-	}
-}
+#task CheckMsBuildPath {
+#	$envPath = $Env:Path
+#	if($envPath.Contains("C:\Windows\Microsoft.NET\Framework\v4.0") -eq $false)
+#	{
+#		if(Test-Path "C:\Windows\Microsoft.NET\Framework\v4.0.30319")
+#		{
+#			$oldEnvPath = $envPath
+#			$Env:Path = $envPath + ";C:\Windows\Microsoft.NET\Framework\v4.0.30319"
+#		}
+#		else
+#		{
+#			throw "Could not determine path to MSBuild. Make sure you have .NET 4.0.30319 installed"
+#		}
+#	}
+#}
 
 task ? -Description "Helper to display task info" {
 	Write-Documentation
