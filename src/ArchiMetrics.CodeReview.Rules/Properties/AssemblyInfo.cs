47517be0f37feb8065ae8b611f272689101b4948
Jacob Reimers <jacob@reimers.dk>
2017-04-05 08:18:26 +0200

    Update project definition files to use VS2017 format.

--- src/ArchiMetrics.CodeReview.Rules/Properties/AssemblyInfo.cs
+++ src/ArchiMetrics.CodeReview.Rules/Properties/AssemblyInfo.cs
@@ -13,33 +13,6 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyTitle("ArchiMetrics.CodeReview")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Reimers.dk")]
-[assembly: AssemblyProduct("ArchiMetrics")]
-[assembly: AssemblyCopyright("Copyright © Reimers.dk 2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-
-
-// Version information for an assembly consists of the following four values:
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
 [assembly: InternalsVisibleTo("ScriptCs.CodeReview")]
 [assembly: InternalsVisibleTo("ArchiMetrics.Raven")]
 [assembly: InternalsVisibleTo("ArchiMetrics.CodeReview.Rules.Tests")]
